200f7ec5d01eb3151af3c1bde7650b17161a146a
steja6872 <109953165+steja6872@users.noreply.github.com>
2025-06-24 09:25:39 -0400

    Create App.java

---
+++ src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello from App!");
+    }
+
+    public static String getMessage() {
+        return "Hello from JUnit!";
+    }
+}
